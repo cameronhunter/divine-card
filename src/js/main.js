a6c491e3404f4e1853d8b1a7b7c99d98e46a17d7
Cameron Hunter <chunter@twitter.com>
2013-06-30 02:31:05 -0700

    Catching JS errors in main and falling back to static fallback

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,44 @@
 var video = document.getElementById('video');
-var audio = document.getElementById('audio');
 var container = document.getElementById('container');
-var videoSize = Math.max(container.offsetWidth || 0, container.clientWidth || 0, container.scrollWidth || 0);
-var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+var videoSize = Math.max(container['offsetWidth'] || 0, container['clientWidth'] || 0, container['scrollWidth'] || 0);
 
-var AllVideoPlayers = [HTML5VideoPlayer, DivineVideoPlayer, StaticFallbackPlayer];
+try {
+  var audio = document.getElementById('audio');
+  var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
 
-var SupportedVideoPlayers = [];
-for (var i in AllVideoPlayers) if (AllVideoPlayers[i].canPlay(video)) {
-  SupportedVideoPlayers.push(AllVideoPlayers[i]);
+  var AllVideoPlayers = [HTML5VideoPlayer, DivineVideoPlayer, StaticFallbackPlayer];
+
+  var SupportedVideoPlayers = [];
+  for (var i in AllVideoPlayers) if (AllVideoPlayers[i].canPlay(video)) {
+    SupportedVideoPlayers.push(AllVideoPlayers[i]);
+  }
+
+  var VideoPlayer = SupportedVideoPlayers[0];
+
+  var player = new VideoPlayer(video, {
+    size: videoSize
+  });
+
+  audio.onclick = function() {
+    if (player.muted()) {
+      player.unmute();
+      audio.className = 'on';
+    } else {
+      player.mute();
+      audio.className = '';
+    }
+    return false;
+  };
+
+  container.onclick = function() {
+    if (player.paused()) {
+      player.play();
+    } else {
+      player.pause();
+    }
+    return false;
+  };
+} catch(e) {
+  // Catch any errors and fallback
+  new StaticFallbackPlayer(video, {size: videoSize});
 }
-
-var VideoPlayer = SupportedVideoPlayers[0];
-
-var player = new VideoPlayer(video, {
-  size: videoSize
-});
-
-audio.onclick = function() {
-  if (player.muted()) {
-    player.unmute();
-    audio.className = 'on';
-  } else {
-    player.mute();
-    audio.className = '';
-  }
-  return false;
-};
-
-container.onclick = function() {
-  if (player.paused()) {
-    player.play();
-  } else {
-    player.pause();
-  }
-  return false;
-};
